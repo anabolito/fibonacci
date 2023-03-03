52121419fc635bed3bae9c51750e8c51dbc93e4e
Ana Cecilia Grossi Bolito <anac.grossi@icloud.com>
2023-03-03 13:57:49 -0300

    Add project files.

---
+++ fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double ant = 0, atual = 1, n;
+
+        Console.Write("Escreva a posição desejada da sequência Fibonacci: ");
+        n = double.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= n; i++)
+        {
+            Console.Write(ant + ", ");
+            atual = ant + atual;
+            ant = atual - ant;
+        }
+    }
+}
